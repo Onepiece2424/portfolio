0c45c5bdd132b485e3bb36a418378256eec45660
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-07 17:40:22 +0900

    productsコントローラの作成

---
+++ spec/requests/potepan/products_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Potepan::Products", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/potepan/products/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/potepan/products/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
