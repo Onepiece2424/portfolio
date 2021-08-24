60b244157d3336b673382a459eb05e0e5767ab35
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-24 20:45:57 +0900

    require 'rails_helper'の削除

--- spec/requests/spree/product_spec.rb
+++ spec/requests/spree/product_spec.rb
@@ -1,7 +1,4 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Spree::Products", type: :request do
-  it '10 - 1 は 9 になること' do
-    expect(10 - 1).to eq 9
-  end
-end
+# RSpec.describe "Spree::Products", type: :request do
+# end
--- spec/views/potepan/products/show.html.erb_spec.rb
+++ spec/views/potepan/products/show.html.erb_spec.rb
@@ -1,7 +1,4 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "products/show.html.erb", type: :view do
-  it '10 - 1 は 9 になること' do
-    expect(10 - 1).to eq 9
-  end
-end
+# RSpec.describe "products/show.html.erb", type: :view do
+# end
