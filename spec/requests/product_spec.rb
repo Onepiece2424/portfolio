fa818ee6c57ea8e9ee06c43cffe58aac4214d82a
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-24 20:46:33 +0900

    specファイルの作成

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -1,9 +1,6 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-  describe "GET /index" do
-    it '10 - 1 は 9 になること' do
-      expect(10 - 1).to eq 9
-    end
+  it "GET /index" do
   end
 end
