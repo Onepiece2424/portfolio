d3f96a25366786da61100bb832943b4c10b924b1
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-26 06:16:36 +0900

    get "/potepan/products"への書き換え

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/potepan/products/index"
+      get "/potepan/products"
       expect(response).to have_http_status(:success)
     end
   end
