74c773c10896a0e704cf853a229fa91f669435c7
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-26 06:16:12 +0900

    visit potepan_products_pathへの書き換え

--- spec/views/potepan/products/index.html.erb_spec.rb
+++ spec/views/potepan/products/index.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 # RSpec.describe "products/index.html.erb", type: :view do
 RSpec.feature 'トップページの表示' do
   it 'index.html.erbへ行く' do
-    visit potepan_products_index_path
+    visit potepan_products_path
   end
 end
