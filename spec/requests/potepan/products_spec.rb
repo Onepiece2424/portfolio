97e04dc08379c75ee399708bbf47c8444c848231
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-28 09:56:51 +0900

    コメントの削除

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -7,12 +7,4 @@ RSpec.describe "Potepan::Products", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
-  # describe "GET /create" do
-  #   it "returns http success" do
-  #     get "/potepan/products/create"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
 end
