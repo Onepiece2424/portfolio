238573c9fde309ee293abf7faba218fd29a3b895
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-08-29 19:19:22 +0900

    共通処理beforeの作成

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Spree::Products", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    before do
       get "/potepan/index"
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
     it "returns a 200 response" do
-      get "/potepan/index"
       expect(response).to have_http_status "200"
     end
   end
