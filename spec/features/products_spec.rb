1dc217a40d78b9e35dbdbe861ab5c71caf924cb1
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-09-11 15:03:45 +0900

    feature specの削除

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Products', type: :feature do
-  scenario 'トップページのタイトルが適切かどうかの確認' do
-    visit potepan_index_path
-    expect(page).to have_title 'Potepan'
-  end
-end
+# RSpec.describe 'Products', type: :feature do
+#   scenario 'トップページのタイトルが適切かどうかの確認' do
+#     visit potepan_index_path
+#     expect(page).to have_title 'Potepan'
+#   end
+# end
