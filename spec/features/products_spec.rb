23d8aed4997819259798fcb3bac88db69a81ef5e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-09-11 15:18:01 +0900

    circleCIが通らないため削除

--- spec/features/products_spec.rb
+++
@@ -1,8 +0,0 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Products', type: :feature do
-#   scenario 'トップページのタイトルが適切かどうかの確認' do
-#     visit potepan_index_path
-#     expect(page).to have_title 'Potepan'
-#   end
-# end
