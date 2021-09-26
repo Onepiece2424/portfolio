001e1e897839c18a7e4656a717a0f85e40b2f72c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-09-26 14:08:47 +0900

    it同士を切り離す

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -11,9 +11,11 @@ RSpec.describe "Spree::Products", type: :request do
     it 'リクエスト200が返ってくるかどうか確認' do
       expect(response.status).to eq(200)
     end
+
     it '商品名が表示されること' do
       expect(response.body). to include product.name
     end
+    
     it '商品画像が取得できること' do
       product.images.each do |display_image|
         expect(response.body). to eq display_image.attachment(:product)
