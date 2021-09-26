a10f945265be92256cd8363c4e84f5d616695497
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-09-26 12:48:26 +0900

    empty→blankメソッドへの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(page_title)
     base_title = "Potepan"
-    page_title.empty? ? base_title : page_title
+    page_title.blank? ? base_title : page_title
   end
 end
