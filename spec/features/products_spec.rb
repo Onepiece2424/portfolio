595d1beed03f64c1151a97af0893fe54c4f47997
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-10 09:15:00 +0900

    feature specの作成

---
+++ spec/features/products_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature '商品詳細ページ' do
+  given!(:product) { create(:product) }
+  given!(:image) { create(:image) }
+
+  before do
+    product.images << image
+  end
+
+  scenario '画像表示テスト(have_selector)' do
+    visit potepan_product_path(product.id)
+      product.images.each do |image|
+        expect(page).to have_selector ("img,[src$='#{image.filename}']")
+      end
+  end
+end
