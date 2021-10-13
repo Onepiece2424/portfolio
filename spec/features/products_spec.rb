994f0bd9cae8b53d58deb7f5edbe24636bad7c94
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-13 21:08:01 +0900

    feature spec 商品名、商品価格、商品説明のテスト

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -14,4 +14,19 @@ RSpec.feature '商品詳細ページ' do
       expect(page).to have_selector("img,[src$='#{image.filename}']")
     end
   end
+
+  scenario '商品名テスト' do
+    visit potepan_product_path(product.id)
+    expect(page).to have_content product.name
+  end
+
+  scenario '商品価格のテスト' do
+    visit potepan_product_path(product.id)
+    expect(page).to have_content product.price
+  end
+
+  scenario '商品説明のテスト' do
+    visit potepan_product_path(product.id)
+    expect(page).to have_content product.description
+  end
 end
