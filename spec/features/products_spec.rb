cd05f2b9d0f3d16eb1262749f48c231329d25a14
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-16 07:19:43 +0900

    before→backgroundへの書き換え。visitの省略。

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -4,29 +4,26 @@ RSpec.feature '商品詳細ページ' do
   given(:product) { create(:product) }
   given(:image) { create(:image) }
 
-  before do
+  background do
+    visit potepan_product_path(product.id)
     product.images << image
   end
 
   scenario '画像表示テスト(have_selector)' do
-    visit potepan_product_path(product.id)
     product.images.each do |image|
       expect(page).to have_selector("img,[src$='#{image.filename}']")
     end
   end
 
   scenario '商品名テスト' do
-    visit potepan_product_path(product.id)
     expect(page).to have_content product.name
   end
 
   scenario '商品価格のテスト' do
-    visit potepan_product_path(product.id)
     expect(page).to have_content product.price
   end
 
   scenario '商品説明のテスト' do
-    visit potepan_product_path(product.id)
     expect(page).to have_content product.description
   end
 end
