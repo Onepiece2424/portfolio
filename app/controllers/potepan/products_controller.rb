0436f102283a0ab90fe5cd8e1be282205e0a4d9b
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-17 14:56:55 +0900

    title表示の変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @page_title = "#{@product.name} - BIGBAG Store"
   end
 end
