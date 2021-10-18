f8055923c88acae73585f9594fb738e388ff14e7
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-19 06:32:22 +0900

    不要な@page_titleの定義部分の削除

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,5 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @page_title = "#{@product.name} - BIGBAG Store"
   end
 end
