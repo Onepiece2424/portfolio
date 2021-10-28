bf74c976481511bc234c800d4303b9c7fa2087ac
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-10-29 06:51:03 +0900

    requests/potepan/categories_spec.rbの作成

---
+++ spec/requests/potepan/categories_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "Spree::Taxon,Spree::Taxonomy", type: :request do
+  let(:taxonomy) { create(:taxonomy) }
+  let(:taxon) { create(:taxon, taxonomy: taxonomy) }
+  let(:product) { create(:product, taxons: [taxon]) }
+  let(:image) { create(:image) }
+
+  before do
+    product.images << image
+    get potepan_category_url taxon.id
+  end
+
+  describe 'showページの表示' do
+    it 'レスポンスが返ってくること' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'カテゴリー一覧の商品名が表示されること' do
+      expect(response.body). to include taxon.name
+    end
+
+    it 'カテゴリー一覧の商品価格が表示されること' do
+      expect(response.body). to include product.display_price.to_s
+    end
+
+    it "左サイドバーの商品カテゴリーの一覧が表示されること" do
+      expect(response.body).to include taxonomy.root.name
+      taxonomy.taxons.leaves.each do |taxon|
+        expect(response.body).to include taxon.name
+      end
+    end
+
+    it 'カテゴリー一覧商品画像が表示されること' do
+      expect(response.body).to include rails_blob_path(product.images.first.attachment)
+    end
+
+    it '商品タイトルが存在していること' do
+      assert_select "title", "#{taxon.name} - BIGBAG Store"
+    end
+  end
+end
