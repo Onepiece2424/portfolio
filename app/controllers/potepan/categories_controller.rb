ca05241655f658ce637909e44410a8f4fae89f12
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-01 17:50:56 +0900

    使用していない@taxon_productsを削除

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,7 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.includes([:taxons])
     @taxon = Spree::Taxon.find(params[:id])
-    @taxon_products = Spree::Product.in_taxons(@taxon).includes(:master, :variants)
     @products = Spree::Product.in_taxons(@taxon).includes(master: :default_price)
   end
 end
