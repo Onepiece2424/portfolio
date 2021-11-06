6d2409be26f0cabf6c3afe705565161738f27578
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-06 15:27:47 +0900

    includesメソッドに関連名を追加し、SQLクエリのアクセス数を制御

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,7 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.includes([:taxons])
     @taxon = Spree::Taxon.find(params[:id])
-    @products = Spree::Product.in_taxons(@taxon).includes(master: :default_price)
+    @products = Spree::Product.in_taxons(@taxon).
+      includes(master: [:default_price, images: [attachment_attachment: :blob]])
   end
 end
