3af426c36b71a48e1578a26f84bfb216b8be1732
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-08 21:01:03 +0900

    @related_productの設定

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_PRODUCTS_COUNT = 4
   def show
     @product = Spree::Product.find(params[:id])
+    @related_product = Spree::Product.related_products(@product).limit(MAX_PRODUCTS_COUNT)
   end
 end
