f4d523bc2e4c826b558014ba966941e9605a5dee
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-09 19:44:47 +0900

    不要なファイルの削除

--- app/models/spree/product_decorator.rb
+++
@@ -1,8 +0,0 @@
-module Spree::ProductDecorator
-  def self.prepended(base)
-    base.scope :related_products, ->(product) {in_taxons(product.taxons).
-    where.not(id: product.id).distinct }
-  end
-
-  Spree::Product.prepend self
-end
