dd3f59e5124fa57d62e322f2a8f5bc2139906ee9
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-10 20:59:45 +0900

    @related_productの変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,8 @@ class Potepan::ProductsController < ApplicationController
   MAX_PRODUCTS_COUNT = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_product = Spree::Product.related_products(@product).limit(MAX_PRODUCTS_COUNT)
+    @related_product = Spree::Product.in_taxons(@product.taxons).
+      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
+      where.not(id: @product.id).limit(MAX_PRODUCTS_COUNT).distinct
   end
 end
