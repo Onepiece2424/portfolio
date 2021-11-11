b5013974532c910637864e775a0c6b2c1d89440e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-11 21:11:19 +0900

    request spec,関連商品名、価格、画像表示のテストの追加

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Spree::Products", type: :request do
   let(:product) { create(:product, taxons: [taxon]) }
-  let(:taxon) { create(:taxon) }
+  let(:taxon) { create(:taxon, taxonomy: taxonomy) }
+  let(:related_product) { create(:product, taxons: [taxon]) }
+  let(:image) { create(:image) }
+  let(:taxonomy) { create(:taxonomy) }
 
   before do
+    related_product.images << image
     get potepan_product_url product.id
   end
 
@@ -27,6 +31,20 @@ RSpec.describe "Spree::Products", type: :request do
 
     it '商品タイトルが存在していること' do
       assert_select "title", "#{product.name} - BIGBAG Store"
+    end
+  end
+
+  describe 'showページの表示（関連商品部分のテスト）' do
+    it '関連商品部分の商品名が表示されること' do
+      expect(response.body). to include related_product.name
+    end
+
+    it '関連商品部分の商品価格が表示されること' do
+      expect(response.body). to include related_product.display_price.to_s
+    end
+
+    it '関連商品の商品画像が表示されること' do
+      expect(response.body). to include rails_blob_path(related_product.images.first.attachment)
     end
   end
 end
