4fcd0eba3618ccc595243bb7689ce9547293e798
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-13 11:11:48 +0900

    related_productの商品名、価格、画像の表示テストの追加(feature spec)

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature '商品詳細ページ' do
   given(:product) { create(:product, taxons: [taxon]) }
   given(:image) { create(:image) }
-  given(:taxon) { create(:taxon) }
+  given(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
+  given(:image) { create(:image) }
+  given(:taxonomy) { create(:taxonomy) }
   given(:related_product) { create(:product, taxons: [taxon]) }
 
   background do
@@ -11,67 +13,89 @@ RSpec.feature '商品詳細ページ' do
     visit potepan_product_path(product.id)
   end
 
-  scenario '画像表示テスト(have_selector)' do
-    product.images.each do |image|
-      expect(page).to have_selector("img,[src$='#{image.filename}']")
+  feature '商品詳細（showページ）ページのテスト(feature spec)' do
+    scenario '画像表示テスト(have_selector)' do
+      product.images.each do |image|
+        expect(page).to have_selector("img,[src$='#{image.filename}']")
+      end
+    end
+
+    scenario '商品名テスト(div.page-title部分のproduct.nameのテスト)' do
+      within(:css, 'div.page-title') do
+        expect(page).to have_content product.name
+      end
+    end
+
+    scenario '商品名テスト(ol.breadcrumb部分のproduct.nameのテスト)' do
+      within(:css, 'ol.breadcrumb') do
+        expect(page).to have_content product.name
+      end
+    end
+
+    scenario '商品名テスト(div.media-body部分のproduct.nameのテスト)' do
+      within(:css, 'div.media-body') do
+        expect(page).to have_content product.name
+      end
+    end
+
+    scenario '商品価格のテスト' do
+      expect(page).to have_content product.price
+    end
+
+    scenario '商品説明のテスト' do
+      expect(page).to have_content product.description
+    end
+
+    scenario 'have_linkを用いた商品リンク(Home)のテスト' do
+      expect(page).to have_link 'Home'
+    end
+
+    scenario 'have_cssを用いた商品リンク(Home、show.html.erb記載)のテスト' do
+      find("section.lightSection").click
+      expect(page).to have_css '.breadcrumb'
+    end
+
+    scenario 'have_cssを用いた商品リンク(Home、_header.html.erb記載)のテスト' do
+      find("div.collapse").click
+      expect(page).to have_css '.active'
+    end
+
+    scenario '商品リンク(BIGBAG)のテスト' do
+      expect(page).to have_css '.navbar-brand'
+    end
+
+    scenario '「一覧ページへ戻る」リンクをクリック後、カテゴリー一覧ページへ移動することを確認するテスト' do
+      click_on '一覧ページへ戻る'
+      expect(page).to have_current_path potepan_category_path(product.taxons.first.id)
     end
   end
 
-  scenario '商品名テスト(div.page-title部分のproduct.nameのテスト)' do
-    within(:css, 'div.page-title') do
-      expect(page).to have_content product.name
+  feature '商品詳細ページ、「関連商品(related_product)」部分のテスト(feature spec)' do
+    background do
+      related_product.images.push(create(:image))
+      visit potepan_product_path(product.id)
     end
-  end
 
-  scenario '商品名テスト(ol.breadcrumb部分のproduct.nameのテスト)' do
-    within(:css, 'ol.breadcrumb') do
-      expect(page).to have_content product.name
+    scenario '関連商品(related_product)部分の商品名、価格、画像の表示テスト(feature spec)' do
+      expect(page).to have_content related_product.name
+      expect(page).to have_content related_product.display_price
+      related_product.images.each do |image|
+        expect(page).to have_selector("img,[src$='#{image.filename}']")
+      end
     end
-  end
 
-  scenario '商品名テスト(div.media-body部分のproduct.nameのテスト)' do
-    within(:css, 'div.media-body') do
-      expect(page).to have_content product.name
+    scenario '関連商品(related_product)の商品名(related_product.name)をクリック後、商品詳細ページへ移動すること' do
+      within('.productBox') do
+        click_on related_product.name
+      end
+      expect(page).to have_current_path potepan_product_path(related_product.id)
     end
-  end
 
-  scenario '商品価格のテスト' do
-    expect(page).to have_content product.price
-  end
-
-  scenario '商品説明のテスト' do
-    expect(page).to have_content product.description
-  end
-
-  scenario 'have_linkを用いた商品リンク(Home)のテスト' do
-    expect(page).to have_link 'Home'
-  end
-
-  scenario 'have_cssを用いた商品リンク(Home、show.html.erb記載)のテスト' do
-    find("section.lightSection").click
-    expect(page).to have_css '.breadcrumb'
-  end
-
-  scenario 'have_cssを用いた商品リンク(Home、_header.html.erb記載)のテスト' do
-    find("div.collapse").click
-    expect(page).to have_css '.active'
-  end
-
-  scenario '商品リンク(BIGBAG)のテスト' do
-    expect(page).to have_css '.navbar-brand'
-  end
-
-  scenario '「一覧ページへ戻る」リンクをクリック後、カテゴリー一覧ページへ移動することを確認するテスト' do
-    click_on '一覧ページへ戻る'
-    expect(page).to have_current_path potepan_category_path(product.taxons.first.id)
-  end
-
-  scenario 'faeture spec 関連商品部分の商品名の表示テスト' do
-    visit potepan_product_url(related_product.id)
-    expect(page).to have_content related_product.name
-  end
-
-  scenario 'faeture spec 関連商品部分の商品価格の表示テスト' do
-    expect(page).to have_content related_product.display_price
+    scenario '関連商品(related_product)の商品価格(related_product.display_price)をクリック後、商品詳細ページへ移動すること' do
+      within('.productBox') do
+        click_on related_product.display_price
+      end
+      expect(page).to have_current_path potepan_product_path(related_product.id)
+    end
   end
 end
