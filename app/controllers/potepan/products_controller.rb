7fbb13b27f29d6b4ce6dd7cef5dde0fc931f93da
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-17 07:20:20 +0900

    contorollerのメソッド部分をdecoratorへ記載。

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,8 +2,6 @@ class Potepan::ProductsController < ApplicationController
   MAX_PRODUCTS_COUNT = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_product = Spree::Product.in_taxons(@product.taxons).
-      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
-      where.not(id: @product.id).limit(MAX_PRODUCTS_COUNT).distinct
+    @related_products = Spree::Product.related_products(@product).limit(MAX_PRODUCTS_COUNT)
   end
 end
