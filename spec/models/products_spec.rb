0b33fe0987ce8da11d162a6789b5a3a6709fe7a4
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-20 11:16:51 +0900

    spec/models/products_spec.rbの作成

---
+++ spec/models/products_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Spree::Products(model spec)", type: :model do
+  let(:product) { create(:product) }
+  let(:taxonomy) { create(:taxonomy) }
+  let(:taxon) { create(:taxon, taxonomy: taxonomy) }
+  let(:related_product1) { create(:product, taxons: [taxon]) }
+  let(:related_product2) { create(:product, taxons: [taxon]) }
+  let(:related_product3) { create(:product, taxons: [taxon]) }
+  let(:related_product4) { create(:product, taxons: [taxon]) }
+  let(:related_product_lists) { create_list(:product, 4, taxons: [taxon]) }
+  let(:related_5products) { create_list(:product, 5, taxons: [taxon]) }
+
+  describe "related_productsメソッドのテスト" do
+    it "related_productsメソッドを用いた関連商品に個々の商品を含んでいること" do
+      expect(product.related_products).to include related_product1
+      expect(product.related_products).to include related_product2
+      expect(product.related_products).to include related_product3
+      expect(product.related_products).to include related_product4
+    end
+
+    it "related_productsメソッドを用いて、商品詳細ページの商品以外を表示すること" do
+      expect(product.related_products).not_to eq product
+    end
+
+    it "related_product_listsの中に、related_productsメソッドを用いて取得した関連商品を取得できていること" do
+      expect(product.related_products).to eq related_product_lists
+    end
+  end
+end
