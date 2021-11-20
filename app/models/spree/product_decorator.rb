41cc969bff729fabe54aff6b5ade6d43aa16be7d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-20 11:18:45 +0900

    decorator,controllerの書き換え

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,10 +1,15 @@
 module Spree::ProductDecorator
-  def self.prepended(base)
-    base.scope :related_products, ->(product) {
-      in_taxons(product.taxons).
-        includes(master: [:default_price, images: [attachment_attachment: :blob]]).
-        where.not(id: product.id).distinct
-    }
+  MAX_PRODUCTS_COUNT = 4
+  def related_products
+    Spree::Product.joins(:taxons).
+      # includes(master: [:default_price, :images]).
+      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
+      select('spree_products.*, spree_products_taxons.taxon_id').
+      distinct.
+      in_taxons(:taxons).
+      where.not(id: id).
+      order(:taxon_id).
+      limit(MAX_PRODUCTS_COUNT)
   end
   Spree::Product.prepend self
 end
