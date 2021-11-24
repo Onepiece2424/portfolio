e3423b555e1cb84bd51210a393e26b282724a6be
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-24 19:25:53 +0900

    decorator記載のincludesメソッド部分をproducts_controllerへ移行

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,6 @@
 module Spree::ProductDecorator
   def related_products
     Spree::Product.joins(:taxons).
-      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
       select('spree_products.*, spree_products_taxons.taxon_id').
       distinct.
       in_taxons(:taxons).
