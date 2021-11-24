5a86b6bc50f8dee0a6a4d1ede581ca4df01ee2e7
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-11-24 20:48:48 +0900

    インデントの修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,8 @@ class Potepan::ProductsController < ApplicationController
   MAX_PRODUCTS_COUNT = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.includes(master: [:default_price, images: [attachment_attachment: :blob]]).limit(MAX_PRODUCTS_COUNT)
+    @related_products = @product.related_products.
+      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
+      limit(MAX_PRODUCTS_COUNT)
   end
 end
